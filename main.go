f91dce5909fceccf7a51d312ba31eaabc2d8f91d
Chris G <griggsca91@gmail.com>
2017-12-05 21:00:41 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/gorilla/sessions"
+	"log"
+	"net/http"
+)
+
+var store = sessions.NewCookieStore([]byte("something-very-secret"))
+
+func CreateSession(c *gin.Context) {
+	// Get a session. We're ignoring the error resulted from decoding an
+	// existing session: Get() always returns a session, even if empty.
+	session, _ := store.Get(c.Request, "session-name")
+	// Set some session values.
+
+	log.Println(session)
+
+	session.Values["foo"] = "bar"
+	session.Values[42] = 43
+	// Save it before we write to the response/return from the handler.
+	session.Save(c.Request, c.Writer)
+}
+
+func main() {
+	// Creates a router without any middleware by default
+	r := gin.New()
+
+	// Global middleware
+	// Logger middleware will write the logs to gin.DefaultWriter even you set with GIN_MODE=release.
+	// By default gin.DefaultWriter = os.Stdout
+	r.Use(gin.Logger())
+
+	// Recovery middleware recovers from any panics and writes a 500 if there was one.
+	r.Use(gin.Recovery())
+
+	// Authorization group
+	// authorized := r.Group("/", AuthRequired())
+	// exactly the same as:
+	authorized := r.Group("/")
+	// per group middleware! in this case we use the custom created
+	// AuthRequired() middleware just in the "authorized" group.
+	authorized.Use(AuthRequired())
+	{
+		authorized.GET("/", homepageEndpoint)
+	}
+
+
+	r.GET("/login", loginEndpoint)
+	// Listen and serve on 0.0.0.0:8080
+	r.Run(":8080")
+}
+
+func loginEndpoint(c *gin.Context) {
+	var msg struct {
+		Endpoint string
+	}
+	msg.Endpoint = "loginEndpoint"
+
+	c.JSON(http.StatusOK, msg)
+
+}
+
+func homepageEndpoint(c *gin.Context) {
+	var msg struct {
+		Endpoint string
+	}
+	msg.Endpoint = "homepageEndpoint"
+
+	c.JSON(http.StatusOK, msg)
+
+}
+
+
+func AuthRequired() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		log.Println("In the AuthRequired")
+		c.Redirect(http.StatusTemporaryRedirect, "/login")
+	}
+}
---
+++ user.go
@@ -0,0 +1,20 @@
+package main
+
+type User struct {
+	Username string
+	HashedPassword string
+}
+
+func ValidateCredentials(username, password string) bool {
+
+
+
+	return false
+}
+
+func GetUser(username string) *User {
+
+
+
+	return nil
+}
