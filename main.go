57ef43e145cdb0920ee84f898f4434102d94e778
Chris <griggsca91@gmail.com>
2017-12-06 20:37:20 -0500

    update

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/gorilla/sessions"
 	"log"
 	"net/http"
+	tb "twitchboard/twitchboard"
+	"github.com/go-pg/pg"
 )
 
 var store = sessions.NewCookieStore([]byte("something-very-secret"))
@@ -24,9 +26,46 @@ func CreateSession(c *gin.Context) {
 	session.Save(c.Request, c.Writer)
 }
 
+func CreateSchema(db *pg.DB) error {
+	for _, model := range []interface{}{&tb.User{}} {
+		err := db.CreateTable(model, nil)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+
+}
+
+
 func main() {
 	// Creates a router without any middleware by default
 	r := gin.New()
+
+	db := tb.DB()
+	log.Print(db)
+
+	err := CreateSchema(db)
+	if err != nil {
+		panic(err)
+	}
+	user1 := &tb.User{
+		Username:   "admin",
+		Email: "admin1@admin",
+	}
+	err = db.Insert(user1)
+	if err != nil {
+		panic(err)
+	}
+
+	var users []tb.User
+	err = db.Model(&users).Select()
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println(users)
+
 
 	// Global middleware
 	// Logger middleware will write the logs to gin.DefaultWriter even you set with GIN_MODE=release.
@@ -47,13 +86,20 @@ func main() {
 		authorized.GET("/", homepageEndpoint)
 	}
 
-
-	r.GET("/login", loginEndpoint)
+	r.POST("/login", postLoginEndpoint)
+	r.GET("/login", getLoginEndpoint)
 	// Listen and serve on 0.0.0.0:8080
 	r.Run(":8080")
 }
 
-func loginEndpoint(c *gin.Context) {
+func postLoginEndpoint(c *gin.Context) {
+
+
+
+	c.Redirect(http.StatusTemporaryRedirect, "/")
+}
+
+func getLoginEndpoint(c *gin.Context) {
 	var msg struct {
 		Endpoint string
 	}
---
+++ twitchboard/user.go
@@ -0,0 +1,26 @@
+package twitchboard
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	)
+
+type User struct {
+	Id int64
+	Username string `sql:",pk"`
+	HashedPassword string
+	Email string `sql:",pk"`
+}
+
+func ValidateCredentials(username, password string) bool {
+	user := GetUser(username)
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.HashedPassword), []byte(password))
+	return err == nil
+}
+
+func GetUser(username string) *User {
+
+
+
+	return nil
+}
